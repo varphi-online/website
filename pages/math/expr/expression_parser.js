ce2447484b2fa40c26078a0d13ec013d284e1728
varphi-online <nbash1984@gmail.com>
2024-06-06 16:48:57 -0700

    lexer fully functional

--- pages/math/expr/expression_parser.js
+++ pages/math/expr/expression_parser.js
@@ -1,267 +1,322 @@
 //Top down recursive descent
 //https://www.youtube.com/watch?v=SToUyjAsaFk
 
-const testExpression = "(3+6)*-5";
+class Tree {
+    constructor() {
 
-class Tree {}
-
-class Num extends Tree {
-  //Number
-  constructor(value) {
-    super();
-    this.value = value;
-  }
-
-  toString() {
-    return this.value.toString();
-  }
-
-  eval() {
-    return this.value;
-  }
+    }
+}
+class TreeNode extends Tree {
+    constructor() {
+        super();
+        this.parent = null;
+    }
 }
 
-class ID extends Tree {
-  //Variable
-  constructor(name) {
-    super();
-    this.name = name;
-    this.value = null;
-  }
+class Num extends TreeNode {
+    //Number
+    constructor(value = "none") {
+        super();
+        this.value = value;
+    }
 
-  toString() {
-    return this.name + "==>" + this.value.toString();
-  }
+    toString() {
+        return this.value.toString();
+    }
 
-  eval() {
-    return this.value;
-  }
+    eval() {
+        return this.value;
+    }
 }
 
-class Add extends Tree {
-  //Add
-  constructor(left, right) {
-    super();
-    this.left = left;
-    this.right = right;
-  }
+class ID extends TreeNode {
+    //Variable
+    constructor(name = "none") {
+        super();
+        this.name = name;
+        this.value = "none";
+    }
 
-  toString() {
-    return "(" + this.left.toString() + "+" + this.right.toString() + ")";
-  }
+    toString() {
+        return this.name + "==>" + this.value.toString();
+    }
 
-  eval() {
-    return this.left.eval() + this.right.eval();
-  }
+    eval() {
+        return this.value;
+    }
 }
 
-class Sub extends Tree {
-  //Subtract
-  constructor(left, right) {
-    super();
-    this.left = left;
-    this.right = right;
-  }
+class Add extends TreeNode {
+    //Add
+    constructor(left = "none", right = "none") {
+        super();
+        this.left = left;
+        this.right = right;
+    }
 
-  toString() {
-    return "(" + this.left.toString() + "-" + this.right.toString() + ")";
-  }
+    toString() {
+        if (this.left)
+            return "(" + this.left.toString() + "+" + this.right.toString() + ")";
+    }
 
-  eval() {
-    return this.left.eval() - this.right.eval();
-  }
+    eval() {
+        return this.left.eval() + this.right.eval();
+    }
 }
 
-class Mult extends Tree {
-  //Multiply
-  constructor(left, right) {
-    super();
-    this.left = left;
-    this.right = right;
-  }
+class Sub extends TreeNode {
+    //Subtract
+    constructor(left = "none", right = "none") {
+        super();
+        this.left = left;
+        this.right = right;
+    }
 
-  toString() {
-    return "(" + this.left.toString() + "+" + this.right.toString() + ")";
-  }
+    toString() {
+        return "(" + this.left.toString() + "-" + this.right.toString() + ")";
+    }
 
-  eval() {
-    return this.left.eval() * this.right.eval();
-  }
+    eval() {
+        return this.left.eval() - this.right.eval();
+    }
 }
 
-class Div extends Tree {
-  //Divide
-  constructor(left, right) {
-    super();
-    this.left = left;
-    this.right = right;
-  }
+class Mult extends TreeNode {
+    //Multiply
+    constructor(left = "none", right = "none") {
+        super();
+        this.left = left;
+        this.right = right;
+    }
 
-  toString() {
-    return "(" + this.left.toString() + "+" + this.right.toString() + ")";
-  }
+    toString() {
+        return "(" + this.left.toString() + "+" + this.right.toString() + ")";
+    }
 
-  eval() {
-    return this.left.eval() / this.right.eval();
-  }
+    eval() {
+        return this.left.eval() * this.right.eval();
+    }
 }
 
-class Neg extends Tree {
-  //Negate
-  constructor(arg) {
-    super();
-    this.arg = arg;
-  }
+class Div extends TreeNode {
+    //Divide
+    constructor(left = "none", right = "none") {
+        super();
+        this.left = left;
+        this.right = right;
+    }
 
-  toString() {
-    return "(-" + this.arg.toString() + ")";
-  }
+    toString() {
+        return "(" + this.left.toString() + "/" + this.right.toString() + ")";
+    }
 
-  eval() {
-    return -this.left.eval();
-  }
+    eval() {
+        return this.left.eval() / this.right.eval();
+    }
 }
+
+class Neg extends TreeNode {
+    //Negate
+    constructor(arg = "none") {
+        super();
+        this.arg = arg;
+    }
+
+    toString() {
+        return "(-" + this.arg.toString() + ")";
+    }
+
+    eval() {
+        return -this.arg.eval();
+    }
+}
+
+class Sqrt extends TreeNode {
+    //Negate
+    constructor(arg = "none") {
+        super();
+        this.arg = arg;
+    }
+
+    toString() {
+        return "(√" + this.arg.toString() + ")";
+    }
+
+    eval() {
+        return Math.sqrt(this.arg.eval());
+    }
+}
+
 
 function tokenize(string) {
-  let tokens = [];
-  let stringIndex = 0;
-  for (let stringIndex = 0; stringIndex < string.length; stringIndex++) {
-    // Working character from input string
-    let currentChar = string[stringIndex];
+    // Map key strings to token types
+    let multiCharNTerminal = new Map();
 
-    // Index [-1] of tokens
-    let lastIndex = tokens.length - 1;
+    multiCharNTerminal.set("sqrt", Sqrt);
+    multiCharNTerminal.set("-", Sub);
+    multiCharNTerminal.set("+", Add);
+    multiCharNTerminal.set("*", Mult);
+    multiCharNTerminal.set("/", Div);
 
-    // Non-terminal characters are pushed to the token array, otherwise do else
-    if ("+-*/^()".includes(currentChar)) {
-      tokens.push(currentChar);
-    } else {
-      // Push an empty string to token stream for mutation if current string is part of a terminal and preceding token is non-terminal
-      if ("+-*/^()".includes(tokens[lastIndex])) {
-        tokens.push("");
-        lastIndex++;
-      }
+    // Grouping characters
+    let groupingChars = '(){}[]';
+    let tokens = [];
+    let stringIndex = 0;
+    for (let stringIndex = 0; stringIndex < string.length; stringIndex++) {
+        // Working character from input string
+        let currentChar = string[stringIndex];
 
-      // If current character is part of an <ID> token, it must be alphabetic
-      if (currentChar.match(/[a-z]|[A-Z]/i)) {
+        // Index [-1] of tokens
+        let lastIndex = tokens.length > 0 ? tokens.length - 1 : 0;
 
-        // If previous term was numeric, we want to make a new token entry into the stream
-        if (parseFloat(tokens[lastIndex]) === NaN) {
-          tokens.push("");
-          lastIndex++;
-          tokens[lastIndex] += currentChar;
+        // Non-terminal characters are pushed to the token array, otherwise do else
+        if (
+            multiCharNTerminal.has(currentChar) ||
+            groupingChars.includes(currentChar)
+        ) {
+            if (multiCharNTerminal.has(currentChar)) {
+                tokens.push(new (multiCharNTerminal.get(currentChar))());
+            } else {
+                tokens.push(currentChar);
+            }
         } else {
-          tokens[lastIndex] += currentChar;
+            // Push an empty string to token stream for mutation if current string is part of a terminal and preceding token is non-terminal
+            if (
+                groupingChars.includes(tokens[lastIndex]) ||
+                (typeof tokens[lastIndex] == "object" &&
+                    Array.from(multiCharNTerminal.values())
+                        .join("")
+                        .includes(tokens[lastIndex].constructor.name))
+            ) {
+                tokens.push("");
+                lastIndex++;
+            } else if (tokens.length == 0) {
+                tokens.push("");
+            }
+
+            // If current character is part of an <ID> token, it must be alphabetic
+            if (currentChar.match(/[a-z]|[A-Z]/i)) {
+                // If previous term was numeric, we want to make a new token entry into the stream
+                if (tokens[lastIndex].length >= 1 && !isNaN(tokens[lastIndex])) {
+                    tokens.push("");
+                    lastIndex++;
+                    tokens[lastIndex] += currentChar;
+                } else {
+                    tokens[lastIndex] += currentChar;
+                }
+            }
+
+            // Check if currentChar is an integer or a decimal point
+            else if (currentChar.match(/[0-9]|\./i)) {
+                tokens[lastIndex] += currentChar;
+            }
+            //All other cases
+            else {
+                tokens[lastIndex] = null;
+            }
         }
-      } 
-      
-      // Check if currentChar is an integer or a decimal point
-      else if (currentChar.match(/[0-9]|\./i)) {
-        tokens[lastIndex] += currentChar;
-      }
-      //All other cases 
-      else {
-        tokens[lastIndex] = null;
-      }
     }
-  }
-  for (let i = 0; i < tokens.length; i++) {
-    if (!"+-*/^()".includes(tokens[i])) {
-      if (isNaN(tokens[i])) {
-        tokens[i] = new ID(tokens[i]);
-      } else {
-        tokens[i] = new Num(tokens[i]);
-      }
+    for (let i = 0; i < tokens.length; i++) {
+        if (multiCharNTerminal.has(tokens[i])) {
+            tokens[i] = new (multiCharNTerminal.get(tokens[i]))();
+        } else if (
+            typeof tokens[i] != "object" &&
+            !groupingChars.includes(tokens[i])
+        ) {
+            if (isNaN(tokens[i])) {
+                tokens[i] = new ID(tokens[i]);
+            } else {
+                tokens[i] = new Num(tokens[i]);
+            }
+        }
     }
-  }
-  return tokens;
+    // End of expression
+    tokens.push("EOE");
+    return tokens;
 }
 
+const testExpression = "(sqrt[x]*3.14159pi)-4";
+
+console.log(tokenize(testExpression));
+
+/*
 function buildTree(expression) {
-    
-  expression = tokenize(expression);
-  console.log(expression)
-  let nextToken = expression[0];
-  let currentToken = expression[-1];
-  let index = -1;
+    expression = tokenize(expression);
+    let resultTree = new Tree();
+    let nextToken = expression[0];
+    let index = -1;
 
-  function scanToken() {
-    currentToken = expression[index];
-    nextToken = expression[index + 1];
-    index++;
-    console.log("Scanned to: ." + currentToken+". , "+nextToken)
-  }
+    function scanToken() {
+        nextToken = expression[index + 1];
+        index++;
+        console.log("Scanned to: ." + nextToken)
+    }
 
-  function parseExpr() {
-    console.log("Expr Parse: " + nextToken)
-    let out = parseTerm();
-    while (true) {
-      if (out === null) {
-        return null;
+    function parseFactor() {
+        console.log("Factor Parse " + nextToken)
+        if (typeof nextToken === ID || typeof nextToken === Num) {
+          return nextToken;
+        } else if (nextToken == "(") {
+          scanToken();
+          let out = parseExpr();
+          if (out === null) {
+            return null;
+          }
+          if (nextToken == ")") {
+            return out;
+          } else {
+            return null;
+          }
+        } else if (nextToken == "-") {
+          scanToken();
+          return new Neg(parseFactor());
+        }  else {
+          return null;
+        }
       }
-      if (nextToken == "+") {
-        scanToken();
-        let second = parseTerm();
-        out = new Add(out, second);
-      } else if (nextToken == "-") {
-        scanToken();
-        let second = parseTerm();
-        out = new Sub(out, second);
-      } else {
-        return out;
+
+      function parseExpr() {
+        console.log("Expr Parse: " + nextToken)
+        let out = parseTerm();
+        while (true) {
+          if (nextToken == "+") {
+            scanToken();
+            let second = parseTerm();
+            out = new Add(out, second);
+          } else if (nextToken == "-") {
+            scanToken();
+            let second = parseTerm();
+            out = new Sub(out, second);
+          } else {
+            return out;
+          }
+        }
+      }
+
+      function parseTerm() {
+        console.log("Term Parse " + nextToken)
+        let out = parseFactor();
+        while (true) {
+        if (nextToken == "*") {
+          scanToken();
+          let second = parseTerm();
+          out = new Mul(out, second);
+        } else if (nextToken == "/") {
+          scanToken();
+          let second = parseTerm();
+          out = new Div(out, second);
+        } else {
+            return out;
+        }
       }
     }
-  }
 
-  //Might have to do while loop here too idk
-
-  function parseTerm() {
-    console.log("Term Parse " + nextToken)
-    let out = currentToken
-    if (nextToken == "*") {
-      scanToken();
-      let second = parseTerm();
-      out = new Mul(out, second);
-      return out;
-    } else if ((nextToken = "/")) {
-      scanToken();
-      let second = parseTerm();
-      out = new Div(out, second);
-      return out;
-    } else {
-      // Var or num
-      out = expression[index]
-      if (out === null) {
-        return null;
-      }
-      return out;
+    while (nextToken != "EOE") {
+        resultTree = parseFactor();
     }
-  }
 
-  function parseFactor() {
-    console.log("Factor Parse " + nextToken)
-    if (typeof nextToken === ID || typeof nextToken === Num) {
-      return nextToken;
-    } else if (nextToken == "(") {
-      scanToken();
-      let out = parseExpr();
-      if (out === null) {
-        return null;
-      }
-      if (nextToken == ")") {
-        return out;
-      } else {
-        return null;
-      }
-    } else if (nextToken == "-") {
-      scanToken();
-      return new Neg(parseFactor());
-    } else {
-      return null;
-    }
-  }
-
-  return parseFactor(expression)
+    return resultTree;
 }
 
 console.log(buildTree(testExpression))
+*/
