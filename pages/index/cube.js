1ec67b28f4c217844d921b31efdf87ed70f7b2cd
varphi-online <nbash1984@gmail.com>
2024-08-21 14:46:55 -0700

    Cube now bounces like DVD logo

--- pages/index/cube.js
+++ pages/index/cube.js
@@ -1,19 +1,59 @@
-var cube = document.querySelector('.cube');
-var rotate = [0,0,0]
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var cube = (document.getElementsByClassName('cube')[0]);
+let container = (document.getElementsByClassName('main')[0]);
+container.style.transform = `translate(0%,0%);`;
+let prim = {
+    windowWidth: window.innerWidth,
+    windowHeight: window.innerHeight,
+    containerWidth: parseInt(getComputedStyle(container).width, 10),
+    containerHeight: parseInt(getComputedStyle(container).height, 10),
+};
+//
+let pos = [parseInt(getComputedStyle(container).left, 10) - prim.containerWidth / 2, parseInt(getComputedStyle(container).top, 10) - prim.containerHeight / 2];
+let dir = [1, -1];
+var rotate = [0, 0, 0];
+let speed = 4;
 function play() {
-  cube.style.transform = "translateZ(-20px) rotateY("+rotate[0] +"deg)" + "rotateX("+rotate[1] +"deg)"+"rotateZ("+rotate[2] +"deg)";
-  rotate[0] = rotate[0]+.6*2;
-  rotate[1] = rotate[1]+.4*2;
+    cube.style.transform = `translateZ(0px) rotateY(${rotate[0]}deg) rotateX(${rotate[1]}deg) rotateZ(${rotate[2]}deg)`;
+    container.style.transform = `translate(0%,0%)`;
+    if (pos[0] + prim.containerWidth > prim.windowWidth || pos[0] <= 0) {
+        dir[0] = dir[0] * -1;
+    }
+    if (pos[1] + prim.containerHeight > prim.windowHeight || pos[1] <= 0) {
+        dir[1] = dir[1] * -1;
+    }
+    pos = [pos[0] + dir[0] * speed, pos[1] + dir[1] * speed];
+    container.style.left = `${pos[0]}px`;
+    container.style.top = `${pos[1]}px`;
+    rotate[0] = rotate[0] + .6 * 2;
+    rotate[1] = rotate[1] + .4 * 2;
 }
-
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-async function pause(){
-await sleep(1000);
-setInterval(play, 100);
+function pause() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield sleep(1000);
+        setInterval(play, 100);
+    });
 }
-
 pause();
+window.addEventListener("resize", () => {
+    prim.windowWidth = window.innerWidth;
+    prim.windowHeight = window.innerHeight;
+    if (pos[0] + prim.containerWidth > prim.windowWidth) {
+        pos[0] = pos[0] + prim.containerWidth - 10;
+    }
+    if (pos[1] + prim.containerHeight > prim.windowHeight) {
+        pos[1] = pos[1] + prim.containerHeight - 10;
+    }
+});
---
+++ pages/index/cube.ts
@@ -0,0 +1,59 @@
+var cube: HTMLDivElement = <HTMLDivElement>(
+  document.getElementsByClassName("cube")[0]
+);
+let container: HTMLDivElement = <HTMLDivElement>(
+  document.getElementsByClassName("main")[0]
+);
+container.style.transform = `translate(0%,0%);`;
+let prim = {
+  windowWidth: window.innerWidth,
+  windowHeight: window.innerHeight,
+  containerWidth: parseInt(getComputedStyle(container).width, 10),
+  containerHeight: parseInt(getComputedStyle(container).height, 10),
+};
+//
+let pos = [
+  parseInt(getComputedStyle(container).left, 10) - prim.containerWidth / 2,
+  parseInt(getComputedStyle(container).top, 10) - prim.containerHeight / 2,
+];
+let dir = [1, 1];
+var rotate = [0, 0, 0];
+let speed = 5;
+
+function play() {
+  cube.style.transform = `translateZ(0px) rotateY(${rotate[0]}deg) rotateX(${rotate[1]}deg) rotateZ(${rotate[2]}deg)`;
+  container.style.transform = `translate(0%,0%)`;
+  if (pos[0] + prim.containerWidth > prim.windowWidth || pos[0] <= 0) {
+    dir[0] = dir[0] * -1;
+  }
+  if (pos[1] + prim.containerHeight > prim.windowHeight || pos[1] <= 0) {
+    dir[1] = dir[1] * -1;
+  }
+  pos = [pos[0] + dir[0] * speed, pos[1] + dir[1] * speed];
+  container.style.left = `${pos[0]}px`;
+  container.style.top = `${pos[1]}px`;
+  rotate[0] = rotate[0] + 0.6 * 2;
+  rotate[1] = rotate[1] + 0.4 * 2;
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function pause() {
+  await sleep(1000);
+  setInterval(play, 100);
+}
+
+pause();
+
+window.addEventListener("resize", () => {
+  prim.windowWidth = window.innerWidth;
+  prim.windowHeight = window.innerHeight;
+  if (pos[0] + prim.containerWidth > prim.windowWidth) {
+    pos[0] = prim.windowWidth - prim.containerWidth - 10;
+  }
+  if (pos[1] + prim.containerHeight > prim.windowHeight) {
+    pos[1] = prim.windowHeight - prim.containerHeight - 10;
+  }
+});
